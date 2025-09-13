373b43120066d7a7355746a572449d456f3e6aa6
牧内　颯汰 <souta.hss.2004@gmail.com>
2025-09-13 22:06:18 +0900

    initial_commit

---
+++ cpp/e/e.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifdef LOCAL_IO
+  freopen("/workspaces/atcoder/cpp/in.txt", "r", stdin);
+  freopen("/workspaces/atcoder/cpp/out.txt", "w", stdout);
+#endif
+
+  cout << "Hello World" << endl;
+  return 0;
+}
