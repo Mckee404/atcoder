017545f6ed40b935680b0a416359b961548db074
牧内　颯汰 <souta.hss.2004@gmail.com>
2025-09-13 22:11:47 +0900

    README

--- cpp/f/f.cpp
+++ cpp/f/f.cpp
@@ -2,11 +2,6 @@
 using namespace std;
 
 int main() {
-#ifdef LOCAL_IO
-  freopen("/workspaces/atcoder/cpp/in.txt", "r", stdin);
-  freopen("/workspaces/atcoder/cpp/out.txt", "w", stdout);
-#endif
-
   cout << "Hello World" << endl;
   return 0;
 }
